36836e59e9a69ac308f90ba86ab96ce8cfe4eb02
Bruno Costa <bruno.costa@dafiti.com.br>
2023-11-09 17:39:56 -0300

    wip

--- src/domain/__tests__/category.entity.spec.ts
+++ src/domain/__tests__/category.entity.spec.ts
@@ -1,8 +1,13 @@
 import {Category} from "../category.entity";
 
 describe("Category Unit Test", () => {
+    let $category: Category;
+
+    beforeEach(() => {
+        $category = Category.create({name: 'testing'});
+    })
+
     test("Checking if you have the id and created_at assigned", () => {
-        const $category: Category = Category.create({name: 'testing'});
         expect($category.id).not.toBeNull();
         expect($category.created_at).toBeInstanceOf(Date)
         expect($category.is_active).toBeTruthy();
@@ -10,29 +15,26 @@ describe("Category Unit Test", () => {
     })
 
     test("Change name field in to category", () => {
-        const $category: Category = Category.create({name: 'testing'});
         expect($category.name).toBe("testing");
         $category.changeName('testing 2');
         expect($category.name).toBe("testing 2");
     })
 
     test("Change description field in to category", () => {
-        const $category: Category = Category.create({name: 'testing'});
         expect($category.description).toBe(null);
         $category.changeDescription('testing');
         expect($category.description).toBe("testing");
     })
 
-    describe("Testing field is_active", function(){
-        test("change field is_active to true", function(){
+    describe("Field is_active", function(){
+        test("Change field is_active to true", function(){
             const $category: Category = Category.create({name: 'testing', is_active: false});
             expect($category.is_active).toBeFalsy();
             $category.enable()
             expect($category.is_active).toBeTruthy();
         })
 
-        test("change field is_active to false", function(){
-            const $category: Category = Category.create({name: 'testing'});
+        test("Change field is_active to false", function(){
             $category.disable()
             expect($category.is_active).toBeFalsy();
         })
