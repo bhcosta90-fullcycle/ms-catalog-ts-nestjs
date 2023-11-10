9c843ef244b08079b01dc724be455a757331c89b
Bruno Costa <bruno.costa@dafiti.com.br>
2023-11-10 14:55:17 -0300

    wip

---
+++ src/@shared/infra/db/in-memory.repository.ts
@@ -0,0 +1,45 @@
+import {IRepository} from "../../domains/repositories/repository-interface";
+import {EntityInterface} from "../../domains/entity.interface";
+import {ValueObjectAbstract} from "../../domains/vo/abstracts/value-object.abstract";
+import {NotFoundError} from "../../domains/exceptions/not-found.error";
+
+export abstract class InMemoryRepository<E extends EntityInterface, EntityId extends ValueObjectAbstract> implements IRepository<E, EntityId> {
+    protected items: E[] = [];
+
+    async bulkInsert(entities: E[]): Promise<void> {
+        this.items.push(...entities);
+    }
+
+    async delete(id: EntityId): Promise<void> {
+        const indexFound: number = this._getId(id);
+        this.items.splice(indexFound, 1);
+    }
+
+    async findAll(): Promise<E[]> {
+        return this.items
+    }
+
+    async findById(id: EntityId): Promise<E> {
+        const item = this.items.find((item) => item.id as any == id)
+        return typeof item === "undefined" ? null : item
+    }
+
+    async insert(entity: E): Promise<void> {
+        this.items.push(entity);
+    }
+
+    async update(entity: E): Promise<void> {
+        const indexFound = this._getId(entity.id);
+        this.items[indexFound] = entity
+    }
+
+    private _getId(id: ValueObjectAbstract): number {
+        const indexFound: number = this.items.findIndex((item) => item.id as any == id)
+        if (indexFound === -1) {
+            throw new NotFoundError(id, this.getEntity());
+        }
+        return indexFound;
+    }
+
+    abstract getEntity(): { new(...args: any[]): E };
+}
