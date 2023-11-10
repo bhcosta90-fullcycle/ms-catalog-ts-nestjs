23dc5d6efd9ff0130a04e9602866a7af64e2962a
Bruno Costa <bruno.costa@dafiti.com.br>
2023-11-10 11:48:40 -0300

    wip

--- src/@shared/vo/unique-id.vo.ts
+++ src/@shared/vo/unique-id.vo.ts
@@ -2,8 +2,6 @@ import {v4 as uuid, validate as validateUuid} from "uuid";
 import {ValueObjectAbstract} from "./abstracts/value-object.abstract";
 
 export class UniqueId extends ValueObjectAbstract {
-    protected _id: string;
-
     constructor(id?: string) {
         super(id ?? uuid());
         this.validate();
