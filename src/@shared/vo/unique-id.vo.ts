6bb9cd3e1072b95601f84bfcd08ad0ef1cea6bc5
Bruno Costa <bruno.costa@dafiti.com.br>
2023-11-10 12:11:14 -0300

    wip

--- src/@shared/vo/unique-id.vo.ts
+++ src/@shared/vo/unique-id.vo.ts
@@ -9,7 +9,14 @@ export class UniqueId extends ValueObjectAbstract {
 
     private validate() {
         if (!validateUuid(this.value)) {
-            throw new Error("ID must be a valid UUID");
+            throw new InvalidUuidError("ID must be a valid UUID");
         }
+    }
+}
+
+export class InvalidUuidError extends Error {
+    constructor(message?: string) {
+        super(message || "ID must be a valida UUID");
+        this.name = "InvalidUuidError";
     }
 }
