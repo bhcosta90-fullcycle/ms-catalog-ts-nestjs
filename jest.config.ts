47b4b6c938c9f7a4bf854accb404e7cd4af87578
Bruno Costa <bruno.costa@dafiti.com.br>
2023-11-10 10:31:28 -0300

    wip

--- jest.config.ts
+++ jest.config.ts
@@ -25,12 +25,12 @@ const config: Config = {
   // collectCoverageFrom: undefined,
 
   // The directory where Jest should output its coverage files
-  // coverageDirectory: undefined,
+  coverageDirectory: './../coverage/',
 
   // An array of regexp pattern strings used to skip coverage collection
-  // coveragePathIgnorePatterns: [
-  //   "/node_modules/"
-  // ],
+  coveragePathIgnorePatterns: [
+    "/node_modules/"
+  ],
 
   // Indicates which provider should be used to instrument code for coverage
   coverageProvider: "v8",
